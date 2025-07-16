a370508a9d18fb301b0425145ff2ba8547aaf2f7
H0AX3N <sandipandebgithub@gmail.com>
2025-07-16 10:46:55 +0530

    add

--- one/src/App.jsx
+++ one/src/App.jsx
@@ -1,34 +1,48 @@
+import { easeInOut, motion } from 'motion/react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div 
+      className='[perspective::1000px] [transform-style:preserve-3d] flex justify-center items-center h-screen w-full bg-neutral-900' 
+      style={{
+        backgroundImage:'radial-gradient(circle at 1px 1px, rgba(255, 255, 255, 0.2) 1px, transparent 0',
+        backgroundSize: '20px 20px',
+        backgroundRepeat: 'repeat',
+      }}
+    >
+      <motion.button 
+        class="group relative text-neutral-500 px-12 py-4 rounded-md bg-black shadow-[inset_0_1px_2px_0_rgba(255,255,255,0.1),inset_0_-1px_2px_0_rgba(255,255,255,0.1)]"
+        initial={{
+          opacity:0
+        }}
+        animate={{
+          opacity:100
+        }}
+        whileHover={{
+          rotateX: 25,
+          rotateY: 10,
+          boxShadow: '0px 20px 50px rgba(8, 112, 184, 0.4)',
+          y: -3
+        }}
+        whileTap={{
+          y: 0
+        }}
+        transition={{
+          duration: 0.3,
+          ease: easeInOut
+        }}
+        style={{
+          translateZ: 100
+        }}
+      >
+        <span className='group-hover:text-cyan-500 duration-800'>Click Me</span>
+        <span className='absolute inset-x-0 bottom-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent h-px w- mx-auto opacity-0 group-hover:opacity-100 transition-opacity duration-300'></span>
+        <span className='absolute inset-x-0 bottom-px bg-gradient-to-r from-transparent via-cyan-400 to-transparent h-[4px] w-full mx-auto blur-sm opacity-0 group-hover:opacity-100 transition-opacity duration-300'></span>
+      </motion.button>
+
+    </div>
   )
 }
 
