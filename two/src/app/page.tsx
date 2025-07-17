945bf5e6125ed7eb6c8b91540e41ffff351f3a47
H0AX3N <sandipandebgithub@gmail.com>
2025-07-17 10:32:28 +0530

    Intital Commit

--- two/src/app/page.tsx
+++ two/src/app/page.tsx
@@ -1,9 +1,10 @@
+import Content from "@/components/Content";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <h1>Hello</h1>
+      <Content />
     </>
   );
 }
